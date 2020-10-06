fb0a4804daff72cbe9ac4cc11f396ba15062d8fa
ase21 <ase911@yandex.ru>
2020-10-06 11:03:59 +0300

    разбираюсь со ScopeFunctions

--- app/src/main/java/com/asefactory/typicodeexample/ui/post_list/PostListActivity.kt
+++ app/src/main/java/com/asefactory/typicodeexample/ui/post_list/PostListActivity.kt
@@ -12,6 +12,12 @@ import com.asefactory.typicodeexample.data.db.entities.PostsCacheEntity
 import com.asefactory.typicodeexample.ui.post_detail.PostDetailActivity
 import com.asefactory.typicodeexample.viewmodel.post_list.PostsListViewModel
 
+class Engine{
+    var name = ""
+    var power = 120
+    var compression = 12.2
+}
+
 class PostListActivity : AppCompatActivity(), PostsListAdapter.OnPostClickListener {
 
     private val postsViewModel : PostsListViewModel by viewModels()
@@ -21,6 +27,39 @@ class PostListActivity : AppCompatActivity(), PostsListAdapter.OnPostClickListen
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_posts_list)
+        //apply
+        //получает текущий контекст объекта
+        //возвращает текущий контекст объекта
+        val someEngine = Engine().apply {
+            this.compression = 10.0
+        }
+
+        //with
+        //получает текущий контекст объекта
+        //возвращает либо результат лямбды, как тут(строка)...
+        val someEnginePower = with(someEngine){
+            println(this.power)
+            "Power of engine is ${this.power}"
+        }
+        //..либо Unit, как тут
+        val someUnitEnginePower = with(someEngine){
+            "Power of engine is ${this.power}"
+            println(this.power)
+        }
+        lateinit var s: String
+        someEngine.also {
+            println("")
+        }
+        println(someEnginePower)
+
+        val someString = someEngine.run{
+            println("sd")
+            "some result"
+        }
+
+        someEngine.let {
+
+        }
 
         val postsListRecyclerView = findViewById<RecyclerView>(R.id.postsListRecyclerView)
         postsListRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
@@ -28,6 +67,7 @@ class PostListActivity : AppCompatActivity(), PostsListAdapter.OnPostClickListen
         postsListRecyclerView.adapter = postsListAdapter
 
         setObservers()
+        Unit
     }
 
     private fun setObservers() {
